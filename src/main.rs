2ef1f1f46c8bb3913eac0dbe3c21505c31ecc309
William Wennerström <william@optmzr.se>
2019-11-03 22:55:33 +0100

    src: test connection with yggdrasil (uncompilable code)

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,32 @@ use actix_files as fs;
 use actix_web::{http, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 use listenfd::ListenFd;
 use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
+use serde_json::Result;
+use std::io::prelude::*;
+use std::os::unix::net::UnixStream;
 
 #[derive(Deserialize)]
 struct PeerForm {
     uri: String,
+}
+
+struct Yggdrasil {
+    stream: UnixStream,
+}
+
+#[derive(Serialize, Deserialize)]
+struct YggRequest {
+    request: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct YggPeers {
+}
+
+#[derive(Serialize, Deserialize)]
+struct YggGetPeersResponse {
+    response:
 }
 
 fn dht_get(_req: HttpRequest) -> impl Responder {
@@ -37,6 +58,30 @@ fn peer_remove(_req: HttpRequest) -> HttpResponse {
     HttpResponse::new(http::StatusCode::NOT_IMPLEMENTED)
 }
 
+fn yggdrasil_send<'de, T>(req: YggRequest) -> &'de T
+where
+    T: serde::Deserialize<'de>,
+{
+    let mut stream = UnixStream::connect("/var/run/yggdrasil.sock").unwrap();
+
+    serde_json::to_writer(&stream, &req).unwrap();
+
+    let mut data = String::new();
+    stream.read_to_string(&mut data).unwrap();
+
+    let response: T = serde_json::from_str(&data.to_owned()).unwrap();
+
+    return &response;
+}
+
+fn yggdrasil_get_peers() -> String {
+    let get_peers = YggRequest {
+        request: "getPeers".to_owned(),
+    };
+
+    return yggdrasil_send(get_peers);
+}
+
 fn main() {
     let mut ssl_acceptor = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
     ssl_acceptor
@@ -56,6 +101,9 @@ fn main() {
             .route("/peer", web::delete().to(peer_remove))
             .service(fs::Files::new("/", "./www/dist").index_file("index.html"))
     });
+
+    let response = yggdrasil_get_peers();
+    println!("{}", response);
 
     server = if let Some(l) = listenfd.take_tcp_listener(0).unwrap() {
         server.listen_ssl(l, ssl_acceptor).unwrap()
