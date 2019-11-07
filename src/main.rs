6b9f63a2c618e93a38ccef87cdaf694a107e266a
William Wennerström <william@optmzr.se>
2019-11-07 15:13:59 +0100

    src: refactor yggdrasil logic into separate module

--- src/main.rs
+++ src/main.rs
@@ -1,116 +1,14 @@
+mod yggdrasil;
+
 use actix_files as fs;
 use actix_web::{http, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 use listenfd::ListenFd;
 use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
-use serde::de::DeserializeOwned;
-use serde::{Deserialize, Serialize};
-use std::borrow::ToOwned;
-use std::collections::HashMap;
-use std::io;
-use std::io::prelude::*;
-use std::os::unix::net::UnixStream;
-use std::result::Result;
-use std::{error::Error, fmt};
-
-const YGG_SOCK: &str = "/var/run/yggdrasil.sock";
-
-#[derive(Debug)]
-enum YggError {
-    Io(io::Error),
-    Json(serde_json::Error),
-}
-
-impl fmt::Display for YggError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match *self {
-            YggError::Io(ref cause) => write!(f, "I/O error: {}", cause),
-            YggError::Json(ref cause) => write!(f, "JSON error: {}", cause),
-        }
-    }
-}
-
-impl Error for YggError {
-    fn description(&self) -> &str {
-        match *self {
-            YggError::Io(ref cause) => cause.description(),
-            YggError::Json(ref cause) => cause.description(),
-        }
-    }
-
-    fn cause(&self) -> Option<&dyn Error> {
-        match *self {
-            YggError::Io(ref cause) => Some(cause),
-            YggError::Json(ref cause) => Some(cause),
-        }
-    }
-}
-
-impl From<io::Error> for YggError {
-    fn from(cause: io::Error) -> YggError {
-        return YggError::Io(cause);
-    }
-}
-
-impl From<serde_json::Error> for YggError {
-    fn from(cause: serde_json::Error) -> YggError {
-        return YggError::Json(cause);
-    }
-}
+use serde::Deserialize;
 
 #[derive(Deserialize)]
 struct PeerForm {
     //uri: String,
-}
-
-#[derive(Serialize, Deserialize)]
-struct YggRequest {
-    request: String,
-}
-
-#[derive(Serialize, Deserialize)]
-struct YggPeers {
-    peers: HashMap<String, YggPeer>,
-}
-
-#[derive(Serialize, Deserialize)]
-struct YggPeer {
-    box_pub_key: String,
-    bytes_recvd: u64,
-    bytes_sent: u64,
-    endpoint: String,
-    port: u16,
-    proto: String,
-    uptime: u64,
-}
-
-#[derive(Serialize, Deserialize)]
-struct YggResponse<T> {
-    response: T,
-    status: String,
-}
-
-fn yggdrasil_send<T>(req: YggRequest) -> Result<T, YggError>
-where
-    T: DeserializeOwned,
-{
-    let mut stream = UnixStream::connect(YGG_SOCK)?;
-
-    serde_json::to_writer(&stream, &req).map_err(YggError::from)?;
-
-    let mut data = String::new();
-    stream.read_to_string(&mut data)?;
-
-    let result: YggResponse<T> = serde_json::from_str(&data.to_owned())?;
-
-    return Ok(result.response);
-}
-
-fn yggdrasil_get_peers() -> Result<YggPeers, YggError> {
-    let get_peers = YggRequest {
-        request: "getPeers".to_owned(),
-    };
-
-    return yggdrasil_send(get_peers);
 }
 
 fn dht_get(_req: HttpRequest) -> impl Responder {
@@ -118,7 +16,7 @@ fn dht_get(_req: HttpRequest) -> impl Responder {
 }
 
 fn peers_get(_req: HttpRequest) -> HttpResponse {
-    let response = yggdrasil_get_peers();
+    let response = yggdrasil::get_peers();
     match response {
         Ok(r) => HttpResponse::Ok().json(r.peers),
         Err(e) => HttpResponse::from_error(actix_web::error::ErrorInternalServerError(e)),
---
+++ src/yggdrasil.rs
@@ -0,0 +1,104 @@
+use serde::de::DeserializeOwned;
+use serde::{Deserialize, Serialize};
+use std::borrow::ToOwned;
+use std::collections::HashMap;
+use std::io;
+use std::io::prelude::*;
+use std::os::unix::net::UnixStream;
+use std::result::Result;
+use std::{error::Error as StdError, fmt};
+
+const DEFAULT_SOCKET: &str = "/var/run/yggdrasil.sock";
+
+#[derive(Debug)]
+pub enum Error {
+    Io(io::Error),
+    Json(serde_json::Error),
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            Error::Io(ref cause) => write!(f, "I/O error: {}", cause),
+            Error::Json(ref cause) => write!(f, "JSON error: {}", cause),
+        }
+    }
+}
+
+impl StdError for Error {
+    fn description(&self) -> &str {
+        match *self {
+            Error::Io(ref cause) => cause.description(),
+            Error::Json(ref cause) => cause.description(),
+        }
+    }
+
+    fn cause(&self) -> Option<&dyn StdError> {
+        match *self {
+            Error::Io(ref cause) => Some(cause),
+            Error::Json(ref cause) => Some(cause),
+        }
+    }
+}
+
+impl From<io::Error> for Error {
+    fn from(cause: io::Error) -> Error {
+        return Error::Io(cause);
+    }
+}
+
+impl From<serde_json::Error> for Error {
+    fn from(cause: serde_json::Error) -> Error {
+        return Error::Json(cause);
+    }
+}
+#[derive(Serialize, Deserialize)]
+struct Request {
+    request: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Peers {
+    pub peers: HashMap<String, Peer>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Peer {
+    pub box_pub_key: String,
+    pub bytes_recvd: u64,
+    pub bytes_sent: u64,
+    pub endpoint: String,
+    pub port: u16,
+    pub proto: String,
+    pub uptime: u64,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Response<T> {
+    response: T,
+    status: String,
+}
+
+fn send<T>(req: Request) -> Result<T, Error>
+where
+    T: DeserializeOwned,
+{
+    let mut stream = UnixStream::connect(DEFAULT_SOCKET)?;
+
+    serde_json::to_writer(&stream, &req).map_err(Error::from)?;
+
+    let mut data = String::new();
+    stream.read_to_string(&mut data)?;
+
+    let result: Response<T> = serde_json::from_str(&data.to_owned())?;
+
+    return Ok(result.response);
+}
+
+pub fn get_peers() -> Result<Peers, Error> {
+    let get_peers = Request {
+        request: "getPeers".to_owned(),
+    };
+
+    return send(get_peers);
+}
